2b5616ec3012c28d9df39cd695ad6a253d716cb6
SerafimNikalayeu <serafim.nikalayeu@digitalinspirationlabs.io>
2023-08-12 16:18:44 +0300

    feat: locations page

--- src/components/Home/Locations.jsx
+++ src/components/Home/Locations.jsx
@@ -1,12 +1,85 @@
 import React from "react";
 import Title from '../Title';
 import Logo from '../Logo';
+import { m } from "framer-motion";
+import LocationsCard from '../LocationsCard';
+
+const heading = {
+    hidden: {},
+    visible: {
+        transition: {
+            staggerChildren: 0.15,
+        },
+    },
+};
+
+const OpenLeft = {
+    hidden: {
+        opacity: 0,
+        transform: 'translate(-500px)'
+    },
+    visible: {
+        opacity: 1,
+        transform: 'translate(0px)',
+        transition: {
+            ease: 'easeOut',
+            duration: 2.5,
+        },
+    },
+};
+
+const OpenRight = {
+    hidden: {
+        opacity: 0,
+        transform: 'translate(500px)'
+    },
+    visible: {
+        opacity: 1,
+        transform: 'translate(0px)',
+        transition: {
+            ease: 'easeOut',
+            duration: 2.5,
+        },
+    },
+};
+
+const locationsData = [
+    {
+        img: '/image/locations-mini-1.png',
+        title: 'ЛЕСНАЯ РЕЗИДЕНЦИЯ',
+        variant: OpenLeft
+    },
+    {
+        img: '/image/locations-mini-2.png',
+        title: 'КОТТЕДЖ',
+        variant: OpenRight
+    },
+    {
+        img: '/image/locations-mini-3.png',
+        title: 'ШАЛЕ',
+        variant: OpenLeft
+    },
+    {
+        img: '/image/locations-mini-4.png',
+        title: 'ОЛИМПИЙСКАЯ ДЕРЕВНЯ',
+        variant: OpenRight
+    },
+];
 
 const Locations = () => {
     return (
         <section className="relative min-h-screen w-full bg-[#201E1F]">
             <Title text={'Локации'} />
             <Logo />
+            <m.div
+                initial='hidden'
+                whileInView='visible'
+                viewport={{ once: true }}
+                variants={heading}
+                className="md:pl-[90px] sm:pl-[40px] pl-3 2xl:pt-[170px] sm:pt-[110px] pt-[90px] pb-6">
+                <m.h2 variants={OpenLeft} className="2xl:text-[68px] xl:text-[58px] lg:text-[40px] sm:text-[28px] text-[22px] text-white font-extrabold sm:pl-0 pl-7">У НАС ЕСТЬ НЕСКОЛЬКО ЛОКАЦИЙ <br /> <span className="text-[#A69376]">СПЕЦИАЛЬНО ДЛЯ ВАС</span></m.h2>
+                <LocationsCard arrayData={locationsData} />
+            </m.div>
         </section>
     );
 }
---
+++ src/components/LocationsCard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { v4 as uuid4 } from 'uuid';
+import { m } from "framer-motion";
+import LocationArrow from './icons/LocationArrow';
+
+const heading = {
+    hidden: {},
+    visible: {
+        transition: {
+            staggerChildren: 0.15,
+        },
+    },
+};
+
+const LocationsCard = ({ arrayData }) => {
+
+    return (
+        <m.div
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true }}
+            variants={heading}
+            className="flex flex-wrap justify-center gap-x-12 items-center md:gap-y-20 gap-y-8 lg:pr-[90px] mt-6">
+            {
+                arrayData.map((el, i) => (
+                    <m.div variants={el.variant} key={uuid4()} className="lg:w-[830px] md:w-[660px] w-[380px] relative group cursor-pointer">
+                        <img className="absolute lg:h-auto md:h-[260px] h-[170px] left-0 bottom-0" src={el.img} alt="" />
+                        <div className="lg:h-[300px] md:h-[235px] h-[155px] w-full bg-[#221C1C] flex flex-col justify-between border border-[#AB8E67] rounded-[30px] lg:pl-[360px] md:pl-[270px] pl-[165px] md:py-6 py-5">
+                            <h4 className="text-[#AB8E67] lg:text-[38px] md:text-[32px] text-xl font-bold">
+                                {el.title}
+                            </h4>
+                            <div className="flex items-center md:gap-3">
+                                <div className="flex flex-col md:gap-6">
+                                    <p className="text-white lg:text-[32px] md:text-[26px] font-semibold">До 30 чел</p>
+                                    <p className="text-white lg:text-[32px] md:text-[26px] font-semibold">от 15 000 руб</p>
+                                </div>
+                                <div className="group-hover:translate-x-6 duration-500 md:w-auto w-[65px]">
+                                    <LocationArrow />
+                                </div>
+                            </div>
+                        </div>
+                    </m.div>
+                ))
+            }
+        </m.div>
+    );
+}
+
+export default LocationsCard;
