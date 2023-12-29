38ce3978aa1c0a62e757a04e5cee3541a59aaef0
OneKekBer <iliaberezovsky227@gmail.com>
2023-12-29 19:12:01 +0100

    done!!

--- src/components/LocationsCard.jsx
+++ src/components/LocationsCard.jsx
@@ -18,16 +18,16 @@ const LocationsCard = ({ arrayData }) => {
     const { t } = useTranslation()
 
     return (
-        <m.div
-            initial='hidden'
-            whileInView='visible'
-            viewport={{ once: false }}
-            variants={heading}
+        <div
+            // initial='hidden'
+            // whileInView='visible'
+            // viewport={{ once: false }}
+            // variants={heading}
             className="flex flex-wrap justify-center gap-x-8 items-center md:gap-y-20 gap-y-8 3xl:pr-[90px] mt-6"
         >
             {arrayData.map((el, i) => (
                 <Link key={uuid4()} to={el.link}>
-                    <m.div variants={el.variant} className="3xl:w-[830px] md:w-[620px] w-[380px] relative group cursor-pointer">
+                    <m.div className="3xl:w-[830px] md:w-[620px] w-[380px] relative group cursor-pointer">
                         <img loading='lazy' className="absolute 3xl:h-auto md:h-[260px] h-[170px] left-0 bottom-0" src={el.imgWebp} alt={el.imgAltText} />
                         <div className="3xl:h-[300px] md:h-[235px] h-[155px] w-full bg-[#221C1C] flex flex-col justify-between border border-[#AB8E67] rounded-[30px] 3xl:pl-[360px] md:pl-[270px] pl-[165px] md:py-6 py-5">
                             <h4 className="text-[#AB8E67] 3xl:text-[38px] md:text-[32px] text-[1.25rem] font-bold">
@@ -50,7 +50,7 @@ const LocationsCard = ({ arrayData }) => {
                     </m.div>
                 </Link>
             ))}
-        </m.div>
+        </div>
     )
 }
 
