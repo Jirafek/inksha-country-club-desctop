2951c8f4501f6253b0bd204e2a23ef04ba05ac4f
OneKekBer <iliaberezovsky227@gmail.com>
2023-12-30 10:55:30 +0100

    langauge cookie

--- src/common/Cookie.jsx
+++ src/common/Cookie.jsx
@@ -1,34 +1,39 @@
-import cookie from "icons/cookies.png";
-import Cookies from "js-cookie";
+import cookie from "icons/cookies.png"
+import Cookies from "js-cookie"
+import { useTranslation } from 'react-i18next'
 
-const Cookie = ({isCookieOpen,setIsCookieOpen, callBack = (isOn) => {}}) => {
-   
+
+const Cookie = ({ isCookieOpen, setIsCookieOpen, callBack = (isOn) => { } }) => {
+   const { t } = useTranslation()
+
    const closeCookie = () => {
-      setIsCookieOpen(false);
-   };
+      setIsCookieOpen(false)
+   }
+
    const handleAccept = () => {
-      Cookies.set('cookies_on', true, { expires: Infinity });
-      localStorage.setItem('cookies_on', 'true');
-      callBack(true);
-      closeCookie();
-      //
-   };
+      Cookies.set('cookies_on', true, { expires: Infinity })
+      localStorage.setItem('cookies_on', 'true')
+      callBack(true)
+      closeCookie()
+   }
+
    const handleReject = () => {
-      closeCookie();
-      callBack(false);
-      //       
-   };
+      closeCookie()
+      callBack(false)
+   }
+
    return (
       <div
-         className={` ${
-            isCookieOpen ? "flex" : "hidden"
-         } bg-white px-[2%] md:px-[5%] gap-3 fixed z-[400000] bottom-0 items-center  w-full h-[145px] justify-between`}
+         className={` ${isCookieOpen ? "flex" : "hidden"
+            } bg-white px-[2%] md:px-[5%] gap-3 fixed z-[400000] bottom-0 items-center  w-full h-[145px] justify-between`}
       >
          <img className="w-[50px] md:w-auto" src={cookie} alt="" />
          <p className="text-small text-black max-w-[600px]">
-            Мы используем файлы cookie. Продолжая использовать наш сайт, вы
-            автоматически соглашаетесь с использованием данных технологий.
-             <a href='/pdf/confi.pdf' className='text-blue-300 underline cursor-pointer'>Политика конфиденциальности</a> .{" "}
+            {t('cookie.message')}
+            <a href='/pdf/confi.pdf' className='text-blue-300 underline cursor-pointer'>
+               {t('cookie.privacyPolicy')}
+            </a>
+            .
          </p>
          <div className="flex items-center flex-col md:flex-row justify-between gap-5">
             <button
@@ -36,7 +41,7 @@ const Cookie = ({isCookieOpen,setIsCookieOpen, callBack = (isOn) => {}}) => {
                className="text-sm underlineOnHover montserrat font-bold"
                type="button"
             >
-               Отклонить
+               {t('cookie.reject')}
             </button>
 
             <button
@@ -44,11 +49,11 @@ const Cookie = ({isCookieOpen,setIsCookieOpen, callBack = (isOn) => {}}) => {
                className="text-sm montserrat hover:border hover:border-black hover:bg-white hover:text-black duration-300 h-[40px] md:h-[50px] w-[100px] md:w-[200px] flex justify-center items-center bg-black text-white font-bold"
                type="button"
             >
-               Принять
+               {t('cookie.accept')}
             </button>
          </div>
       </div>
-   );
-};
+   )
+}
 
-export default Cookie;
+export default Cookie
