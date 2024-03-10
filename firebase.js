771dcf456ae9ce15cf4f98cd9a578f0a3f32d3e5
SerafimNikalayeu <serafim.nikalayeu@humandev.org>
2024-03-10 14:00:22 +0300

    fix: firebase setter

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import firebaseConfig from './firebase-config.js';
 import {initializeApp} from 'firebase/app';
-import { getDatabase, ref, get, set } from 'firebase/database';
+import { getDatabase, ref, get, set, push, update } from 'firebase/database';
 
 const app = initializeApp(firebaseConfig);
 
@@ -39,17 +39,24 @@ export const addUser = async (clientId) => {
             snapshot.forEach((childSnapshot) => {
                 const childData = childSnapshot.val();
                 if (childData.clientId === clientId) {
-                    set(ref(db, `yandexUsers/${childSnapshot.key}`), {
-                        clientId: clientId,
-                        timestamp: Date.now()
-                    });
+
+                    update(usersRef, {
+                        [childSnapshot.key]: {
+                            clientId: clientId,
+                            timestamp: Date.now()
+                        }
+
+                    })
                     isUpdated = true;
                 }
             });
         }
 
         if (!isUpdated) {
-            await set(usersRef, {
+
+            console.log('pushing new user to firebase')
+
+            await push(usersRef, {
                 clientId: clientId,
                 timestamp: Date.now()
             });
